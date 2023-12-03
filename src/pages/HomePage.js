fc23c070dc03d399cdd5d7b2a8555cce495a4a8f
Koundinya <koundinya.g07@gmail.com>
2023-12-03 23:01:46 +0530

    Final Project Code

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Home from '../Components/Home'
+import Display from '../Components/Display'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Home/>
+      <Navbar/>
+      <Display/>
+    </div>
+  )
+}
+
+export default HomePage
